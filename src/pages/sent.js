c8ff2580d735137c632f2bfb6bbfe98ba058df8f
James Mitchell <iamjmitch1990@gmail.com>
2020-10-10 01:10:27 +1000

    mail test 3

---
+++ src/pages/sent.js
@@ -0,0 +1,26 @@
+import React from "react"
+// import { Link } from "gatsby"
+import "../styles/index.css"
+
+import Layout from "../components/layout"
+import Hero from "../components/hero"
+import Who from "../components/who"
+import Tech from "../components/technologies/technologies"
+import Portfolio from "../components/portfolio/portfolio"
+import Contact from "../components/contact"
+
+const indexPage = () => {
+  return (
+    <div>
+      <Layout>
+        <Hero />
+        <Who />
+        <Tech />
+        <Portfolio />
+        <Contact />
+      </Layout>
+    </div>
+  )
+}
+
+export default indexPage
