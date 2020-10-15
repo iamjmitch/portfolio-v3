bd9d1535b08dbdbc9d279684aedab4f818ba0290
James Mitchell <iamjmitch1990@gmail.com>
2020-10-16 01:22:38 +1000

    contact form complete

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,19 +1,34 @@
 import React from "react"
 import styled from "styled-components"
-
+import slideOut from "../images/slide-out.png"
 const FormContainer = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   padding: 100px 0;
+  max-width: 400px;
+  margin: 0 auto;
   form {
-    width: 400px;
+    overflow: visible;
+    position: relative;
+    width: 100%;
     div {
       display: flex;
       flex-direction: column;
+      span {
+        position: absolute;
+        transition: 0.4s;
+        right: 0;
+        bottom: 35px;
+        z-index: 0;
+      }
       label {
         color: #fc2602;
+        margin: 10px 0 3px 0;
         text-transform: uppercase;
+        font-size: 0.9rem;
       }
       input,
       textarea {
@@ -22,14 +37,71 @@ const FormContainer = styled.div`
         padding: 3px 8px;
         color: white;
         text-transform: capitalize;
+        resize: none;
+        z-index: 1;
+      }
+      input[type="email"] {
+        text-transform: lowercase;
+      }
+      textarea {
+        &:focus + span {
+          transform: translateX(100%);
+          bottom: 60px;
+        }
+      }
+    }
+    li {
+      list-style: none;
+      button {
+        background: #fc2602;
+        color: white;
+        min-width: 50px;
+        border: none;
+        font-size: 0.9rem;
+        font-weight: 600;
+        padding: 6px 30px;
+        font-family: "Poppins", sans-serif;
+        margin-top: 10px;
+        cursor: pointer;
+        z-index: 999;
+        align-self: center;
+        transition: all 0.5s;
+        list-style: none;
+        &:hover {
+          color: #fc2602;
+          box-shadow: inset 200px 0px 9px 0px rgb(255 255 255);
+        }
       }
     }
   }
 `
 
+const Heading = styled.div`
+  text-align: center;
+  padding-bottom: 30px;
+  h4 {
+    color: #fc2602;
+    font-size: 1.9rem;
+    font-weight: 600;
+  }
+  h5 {
+    color: #fff;
+    font-size: 0.8rem;
+    font-weight: 500;
+  }
+`
+
+const SlideoutImage = styled.img`
+  height: 130px;
+`
+
 const ContactForm = props => {
   return (
     <FormContainer>
+      <Heading>
+        <h4>CONTACT ME</h4>
+        <h5>* REQUIRED FIELDS</h5>
+      </Heading>
       <form
         method="post"
         netlify-honeypot="bot-field"
@@ -53,12 +125,15 @@ const ContactForm = props => {
         </div>
         <div>
           <label>Message*</label>
-          <textarea name="message" rows="5" required></textarea>
+          <textarea name="message" rows="10" required></textarea>
+          <span>
+            <SlideoutImage src={slideOut} />
+          </span>
         </div>
         <ul className="actions">
           <li>
-            <button type="submit" className="button special">
-              Send
+            <button type="submit" className="button">
+              SEND
             </button>
           </li>
         </ul>
