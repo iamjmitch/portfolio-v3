4c1a88e2de0d4d92eff887fac4462b49015add6f
James Mitchell <iamjmitch1990@gmail.com>
2020-11-08 01:18:50 +1000

    undid some changes no longer needed

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Contact from "../components/contact"
 
 const indexPage = () => {
   return (
-    <div style={{ width: "100vw", maxWidth: "100%" }}>
+    <div>
       <Layout>
         <Hero />
         <Tech />
